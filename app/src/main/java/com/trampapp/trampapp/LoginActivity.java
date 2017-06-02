5313093aebaa7be28942913032570360e3e2239a
pavel03 <pavel03@gmail.com>
2017-06-03 01:06:23 +0300

    Initial commit

---
+++ app/src/main/java/com/trampapp/trampapp/LoginActivity.java
@@ -0,0 +1,55 @@
+package com.trampapp.trampapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    LoginButton loginButton;
+    TextView textView;
+    CallbackManager callbackManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_login);
+        loginButton = (LoginButton)findViewById(R.id.fb_login_btn);
+        textView =(TextView)findViewById(R.id.textview);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                textView.setText("Login Success");
+
+            }
+
+            @Override
+            public void onCancel() {
+                textView.setText("Login Cancelled");
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                textView.setText("Login Error");
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
